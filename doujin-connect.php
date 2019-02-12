a10cd99f7f2cfbf153fe9316a7b2d9c7f5783d3b
sinkaroid <bibsindra@gmail.com>
2019-02-12 10:48:15 +0700

    Update doujin-connect.php

--- doujin-connect.php
+++ doujin-connect.php
@@ -1,31 +1,66 @@
-<title>Doujin Stealer</title>
+<title>Mass Image Grabber</title>
+<link rel="shortcut icon" href="https://1.bp.blogspot.com/-q4WhoEwfRlI/XDCnuof_TsI/AAAAAAAAIsQ/HxA6hd2mIOgiO8KveXaxKwfaecawWpCgwCLcBGAs/s1600/kano.png" type="image/x-icon">
+<meta name='author' content='Stupidc0de Family'>
+<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">
+
+<style>
+
+.subbtn {background:#b70505;color:white;border: 1px solid #000; padding:6px 6px 6px 6px;}
+
+
+.bordergaya{
+			background:black;
+			color:white;
+			margin:0 10px;
+			font-family:Ubuntu Mono;
+			font-size:16px;
+			border:2px solid #2d2b2b;
+	
+		}
+		.bordergaya:hover{
+			background:#2d2b2b;
+			color:white;
+			margin:0 10px;
+			font-family:Ubuntu Mono;
+			font-size:16px;
+			border:2px solid crimson;
+		}
+	
+
+
+</style>
 
 <?php
-	include('crawler.php'); //Parser to get element in your link
+	include('slut.php'); //Parser to get element in your link
 	if(isset($_POST['submit'])){	
 		$url=file_get_html($_POST['url']); 
 	$image = $url->find("img"); 
 	foreach($image as $img) 
 	{
 		$s=$img->src; 
-		$img_name = 'C:\xampp\htdocs\onee/'.basename($s); //change it
+		$img_name = 'result/'.basename($s); //change it
 		file_put_contents($img_name, file_get_contents($s)); 
 	}
 	}
 	
 	
 ?>
-<center><img src='https://4.bp.blogspot.com/-ayMbYggKOUI/XDDElHM2YJI/AAAAAAAAIs8/cM3IrJLfZkkkcMQHHNz6Gq9nKfe9jnBsACLcBGAs/s1600/katousan.png' width='400' height='550'/>
+  <center><font face="Ubuntu" size="5" color="white"><b>Mass Image Grabber by Sc0<b/></font></center>
+  <p>
+  <center><img class='image' src='https://3.bp.blogspot.com/-6XZOk6uPV4U/XEBO8xwFcwI/AAAAAAAABL4/BDAHAxIoOCAZCO0FmEnNtAyGXoTx9dz6wCLcBGAs/s1600/weeblogo.png' width='480' height='420'/>
+<br>
+<center>
+<body bgcolor=black>
 <form id="form1" name="form1" method="post" action="">
-  <table width="500" border="1" align="center" cellpadding="1" cellspacing="1">
+  <table width="300"  align="center" cellpadding="1" cellspacing="1">
     <tr>
       <td colspan="2"><label for="textfield"></label>
-      <input style="width:100%;" type="text" name="url" id="textfield" />
+      <input class="bordergaya" style="width:100%;" placeholder="put your full url here..." type="text" name="url" id="textfield" />
       </td>
-     
+      
     </tr>
     <tr>
-      <td colspan="2" align="center" valign="middle"><input type="submit" name="submit" id="button" value=" > " /></td>
+      <td colspan="2" align="center" valign="middle"><input class="subbtn" type="submit" name="submit" id="button" value="  >  " /></td>
     </tr>
   </table>
 </form>
